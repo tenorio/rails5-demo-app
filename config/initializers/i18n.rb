f9b498686999203ebe3cb6986cf660bc34663465
Marcelo Tenorio de Souza <marcelllo.martin@gmail.com>
2017-01-25 21:13:48 -0200

    Layout improvements

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -2,6 +2,6 @@ I18n.available_locales = [:en, :'pt-BR']
 I18n.default_locale = :'pt-BR'
 
 LANGUAGES = [
-  ['Portugu&ecirc;s do Brasil'.html_safe, 'pt-BR'],
+  ['Portugu&ecirc;s'.html_safe, 'pt-BR'],
   ['English', 'en']
 ]
