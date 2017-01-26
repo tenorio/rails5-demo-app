a01531b846fdb66a3b91b260dcc5884faf6ec828
Marcelo Tenorio de Souza <marcelllo.martin@gmail.com>
2017-01-26 20:53:30 -0200

    Layout improvements

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   validates_length_of :password, minimum: 6
+  validates_presence_of :email
+  validates_confirmation_of :email, message: I18n.t('errors.messages.confirmation')
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
   validates_uniqueness_of :email
 end
